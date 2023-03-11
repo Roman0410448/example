9482d7a550e05ffc944a80a7b12f45240d1d6856
ROMAN <junk0410448@mail.ru>
2023-03-11 15:01:43 +0700

    example 004

---
+++ Example004_Dif/Program.cs
@@ -0,0 +1,6 @@
+﻿double NamberA = new Random().Next(1, 10);
+Console.WriteLine(NamberA);
+double NamberB = new Random().Next(1, 10);
+Console.WriteLine(NamberB);
+double result = NamberA / NamberB;
+Console.WriteLine(result);
